ce67395fdee4490e91e882022765a16ea942e869
Antoine Falgiglio <falgitoine@gmail.com>
2021-01-21 22:40:51 +0100

    Commit de fin de projet

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  movieApiUrl = '';
+  movieData = {
+    title: '',
+    description: '',
+    imageUrl: ''
+  };
 
-  constructor() {}
+  constructor(public http: HttpClient) {
+
+    this.readAPI('http://www.omdbapi.com/?apikey=f727637c&t=Mr+Robot')
+    .subscribe((data) => {
+      console.log(data);
+      console.log(data['Title']);
+      this.movieData.title = data['Title'];
+      this.movieData.description = data['Plot'];
+      this.movieData.imageUrl = data['Poster'];
+    });
+  }
+
+    readAPI(URL: string) {
+      return this.http.get(URL);
+  }
 
 }
